98a35e3a0ea2c95e36f60379797fa75bb45f1be7
Huseyin Orhan <huseyinsoftware@gmail.com>
2025-03-12 14:34:05 +0300

    Authentication

---
+++ backend-services/AuthenticationService/AuthenticationService/Program.cs
@@ -0,0 +1,33 @@
+using AuthenticationService.ServiceRegistration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add json files
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+
+// Add services to the container.
+builder.Services.AddServices(builder.Configuration);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
