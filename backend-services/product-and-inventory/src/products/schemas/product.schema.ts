f69be95ccfcb4a035888195158533f22b7b91cba
Baran Kırca <barankircaa@gmail.com>
2025-03-16 18:03:50 +0300

    add basic product schema, establish local mongo connection

---
+++ backend-services/product-and-inventory/src/products/schemas/product.schema.ts
@@ -0,0 +1,20 @@
+﻿import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type ProductDocument = HydratedDocument<Product>;
+
+@Schema({ timestamps: true })
+export class Product {
+  @Prop()
+  id: mongoose.Types.ObjectId;
+  @Prop()
+  name: string;
+  @Prop()
+  price: number;
+  @Prop()
+  vendor_id: number;
+  @Prop()
+  image: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
