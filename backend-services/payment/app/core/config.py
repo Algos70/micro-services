57e00fb644363a4f8935a71d893954d71c457ac7
Kürşat <draknes.gamin70@gmail.com>
2025-03-27 13:13:49 +0300

    feat: paymenty service rabbitmq publisher

--- backend-services/payment/app/core/config.py
+++ backend-services/payment/app/core/config.py
@@ -15,3 +15,12 @@ AUTHORIZATION_SERVER_PORT = os.getenv("AUTHORIZATION_SERVER_PORT",default=5206)
 AUTHORIZATION_SERVER_CUSTOMER_ENDPOINT = "/customer-policy"
 AUTHORIZATION_SERVER_VENDOR_ENDPOINT = "/vendor-policy"
 AUTHORIZATION_SERVER_ADMIN_ENDPOINT = "/admin-policy"
+
+
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", default="localhost")
+RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT", default=5672))
+RABBITMQ_USER = os.getenv("RABBITMQ_USER", default="guest")
+RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD", default="guest")
+RABBITMQ_PRODUCTS_QUEUE = "products_queue"
+RABBITMQ_ORDERS_QUEUE = "orders_queue"
+RABBITMQ_PAYMENT_QUEUE = "payment_queue"
