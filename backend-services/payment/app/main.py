3fb58ef783def7e7b1cf3972f0f0a2801e047697
Kürşat <draknes.gamin70@gmail.com>
2025-03-27 13:25:02 +0300

    feat: payment rabbitmq consumer

--- backend-services/payment/app/main.py
+++ backend-services/payment/app/main.py
@@ -1,4 +1,5 @@
-import os
+import threading
+from core import config
 from fastapi import FastAPI, Depends
 from db.dependencies import get_db
 from sqlalchemy.orm import Session
@@ -6,13 +7,25 @@ from contextlib import asynccontextmanager
 from db.base import engine, Base
 from entity import payment
 from api.endpoints import payments
-
+from services.rabbitmq_consumer import get_consumer_service
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # Startup: create and start the consumer thread
     Base.metadata.create_all(bind=engine)
     print("Database connected")
-    yield  
+    consumer = get_consumer_service(queue=config.RABBITMQ_ORDERS_QUEUE)
+    thread = threading.Thread(target=consumer.start_consuming, daemon=True)
+    thread.start()
+    print("Consumer thread started.")
+    try:
+        yield
+    finally:
+        # Shutdown: Signal the consumer to stop and wait for the thread to exit
+        consumer.stop_consuming()
+        thread.join(timeout=5)
+        print("Consumer stopped.")
+
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(payments.router)
