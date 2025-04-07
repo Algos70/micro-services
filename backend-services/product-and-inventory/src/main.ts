f62045e761d0b882046b2d424bb06aa0ad9065bb
Baran Kırca <bkirca@192.168.1.17>
2025-04-07 05:22:56 +0300

    add swagger, make hybrid nest app

--- backend-services/product-and-inventory/src/main.ts
+++ backend-services/product-and-inventory/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
@@ -11,24 +12,34 @@ async function bootstrap() {
 
   for (let attempt = 0; attempt < maxRetries; attempt++) {
     try {
-      const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-        AppModule,
-        {
-          transport: Transport.RMQ,
-          options: {
-            urls: [
-              process.env.RABBITMQ_URL || 'amqp://guest:guest@rabbitmq:5672',
-            ],
-            queue: 'products-queue',
-            queueOptions: { durable: true },
-            socketOptions: {
-              heartbeatIntervalInSeconds: 30,
-              reconnectTimeInSeconds: 5,
-            },
+      const app = await NestFactory.create(AppModule);
+
+      const config = new DocumentBuilder()
+        .setTitle('My Hybrid App')
+        .setDescription('API documentation for the HTTP part of the app')
+        .setVersion('1.0')
+        .addTag('products') // optional
+        .build();
+
+      const document = SwaggerModule.createDocument(app, config);
+      SwaggerModule.setup('api', app, document);
+
+      app.connectMicroservice({
+        transport: Transport.RMQ,
+        options: {
+          urls: [
+            process.env.RABBITMQ_URL || 'amqp://guest:guest@rabbitmq:5672',
+          ],
+          queue: 'products-queue',
+          queueOptions: { durable: true },
+          socketOptions: {
+            heartbeatIntervalInSeconds: 30,
+            reconnectTimeInSeconds: 5,
           },
         },
-      );
-      await app.listen();
+      });
+      await app.startAllMicroservices();
+      await app.listen(4040);
       logger.log('Microservice connected to RabbitMQ');
     } catch ({ stack }) {
       logger.error(
