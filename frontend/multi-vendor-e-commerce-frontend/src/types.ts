47913d20fed2ff693a509263d71010515224142f
electus-1 <barankircaa@gmail.com>
2025-05-22 16:47:32 +0300

    update urls

--- frontend/multi-vendor-e-commerce-frontend/src/types.ts
+++ frontend/multi-vendor-e-commerce-frontend/src/types.ts
@@ -3,10 +3,10 @@ export interface SignInFormProps {
 }
 
 export interface Config {
-  ORDER_URL: 'https://65.21.6.50/order';
-  AUTH_URL: 'https://65.21.6.50/auth';
-  CONSUMER_URL: 'https://65.21.6.50/product';
-  ORCHESTRATION_URL: 'https://65.21.6.50/orchestration';
+  ORDER_URL: 'http://localhost:8080';
+  AUTH_URL: 'http://localhost:8086';
+  CONSUMER_URL: 'http://localhost:9292';
+  ORCHESTRATION_URL: 'http://localhost:7001';
 }
 
 export interface CartItem {
